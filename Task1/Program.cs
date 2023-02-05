5e8c5094a85d365196138bd7726e551d8ecfa06c
ElWin95 <elvinma@code.edu.az>
2023-02-06 00:28:13 +0400

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Xml;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //Console.WriteLine("Reqemi daxil edin");
+            //int number = int.Parse(Console.ReadLine());
+            //int countDigitLog = (int)Math.Log10(number) + 1;
+            //int r, i = 0;
+            //int sum = 0;
+
+            //while (number != 0)
+            //{
+            //    r = number % 10;
+            //    number /= 10;
+
+            //    if (i % 2 == 0)
+            //    {
+            //        sum = r * r;
+
+            //        Console.WriteLine(sum);
+
+            //    }
+
+            //    i++;
+
+            //}
+
+            //Console.WriteLine(sum);
+            #endregion
+            #region Task2
+            //Console.WriteLine("Reqemi daxil edin");
+            //int number = int.Parse(Console.ReadLine());
+            //int count = (int)Math.Log10(number) + 1;
+            //int r;
+            //int sum = 0;
+            //while (number != 0)
+            //{
+            //    r = number % 10;
+            //    number /= 10;
+            //    sum += r * count;
+            //    count--;
+
+            //}
+
+            //Console.WriteLine(sum);
+            #endregion
+            #region Task3
+            //Console.WriteLine("Reqemi daxil edin");
+            //int number = int.Parse(Console.ReadLine());
+            //int countDigitLog = (int)Math.Log10(number) + 1;
+            //int r1, r2;
+            //int tmp;
+            //bool isDifferenceDigit = true;
+            //while (number != 0)
+            //{
+            //    r1 = number % 10;
+            //    number /= 10;
+            //    tmp = number;
+            //    while (tmp != 0)
+            //    {
+            //        r2 = tmp % 10;
+            //        tmp /= 10;
+            //        if (r1 == r2)
+            //        {
+            //            Console.WriteLine("Muxtelif reqemli deyil");
+            //            isDifferenceDigit = false;
+            //            break;
+            //        }
+            //    }
+
+            //    if (!isDifferenceDigit)
+            //        break;
+
+            //}
+
+            //if (isDifferenceDigit)
+            //    Console.WriteLine("Muxtelif reqemlidir");
+            #endregion
+            #region Task4
+            //Console.WriteLine("Reqemi daxil edin");
+            //int n = int.Parse(Console.ReadLine());
+            //int count = (int)Math.Log10(n) + 1;
+            //int r, sum = 0;
+
+            //for(int i = count; i >=1; i--)
+            //{
+            //    r = n % 10;
+            //    n /= 10;
+
+            //    if (i % 2 == 1) continue;
+            //    sum += r*r;
+            //}
+
+           
+            //Console.WriteLine(sum);
+            #endregion
+        }
+
+    }
+}
